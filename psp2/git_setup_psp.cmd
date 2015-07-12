c279508e4f0e570faa27556a61b517475a17fd66
CarlosOnline <cgomes@iinet.com>
2015-07-11 22:01:28 -0700

    cache password

---
+++ psp2/git_setup_psp.cmd
@@ -0,0 +1,6 @@
+git remote add origin https://github.com/CarlosOnline/PSP2.git
+git config user.email cgomes@iinet.com
+git config user.name CarlosOnline
+git config credential.https://github.com.username CarlosOnline
+git config --global credential.helper wincred
+git pull -u origin master
