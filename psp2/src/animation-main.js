28f858e5e6de24138646c02ffcfc460900f9bea8
CarlosOnline <cgomes@iinet.com>
2015-07-12 19:59:45 -0700

    Get Typescript working in VS move files up a level

--- psp2/src/animation-main.js
+++ psp2/src/animation-main.js
@@ -1,8 +1,12 @@
-export function configure(aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .developmentLogging()
-    .plugin('aurelia-animator-css');
+define(["require", "exports"], function (require, exports) {
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .eventAggregator()
+            .plugin('aurelia-animator-css');
+        aurelia.start().then(function (a) { return a.setRoot(); });
+    }
+    exports.configure = configure;
+});
 
-  aurelia.start().then(a => a.setRoot());
-}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInZpZXdzL2FuaW1hdGlvbi1tYWluLnRzIl0sIm5hbWVzIjpbImNvbmZpZ3VyZSJdLCJtYXBwaW5ncyI6IjtJQUFBLG1CQUEwQixPQUFPO1FBQy9CQSxPQUFPQSxDQUFDQSxHQUFHQTthQUNSQSxxQkFBcUJBLEVBQUVBO2FBRXZCQSxlQUFlQSxFQUFFQTthQUNqQkEsTUFBTUEsQ0FBQ0Esc0JBQXNCQSxDQUFDQSxDQUFDQTtRQUVsQ0EsT0FBT0EsQ0FBQ0EsS0FBS0EsRUFBRUEsQ0FBQ0EsSUFBSUEsQ0FBQ0EsVUFBQUEsQ0FBQ0EsSUFBSUEsT0FBQUEsQ0FBQ0EsQ0FBQ0EsT0FBT0EsRUFBRUEsRUFBWEEsQ0FBV0EsQ0FBQ0EsQ0FBQ0E7SUFDekNBLENBQUNBO0lBUmUsaUJBQVMsWUFReEIsQ0FBQSIsImZpbGUiOiJ2aWV3cy9hbmltYXRpb24tbWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBjb25maWd1cmUoYXVyZWxpYSkge1xuICBhdXJlbGlhLnVzZVxuICAgIC5zdGFuZGFyZENvbmZpZ3VyYXRpb24oKVxuICAgIC8vLmRldmVsb3BtZW50TG9nZ2luZygpXG4gICAgLmV2ZW50QWdncmVnYXRvcigpXG4gICAgLnBsdWdpbignYXVyZWxpYS1hbmltYXRvci1jc3MnKTtcblxuICBhdXJlbGlhLnN0YXJ0KCkudGhlbihhID0+IGEuc2V0Um9vdCgpKTtcbn1cbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
---
+++ psp2/src/app.ts
@@ -0,0 +1,30 @@
+﻿import { computedFrom, bindable } from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import { Utility } from './views/common/utility';
+import { ItemViewModel } from './views/itemViewModel';
+import { FormEditor } from './views/formEditor';
+import { SnippetForm } from './views/snippet-form';
+///<reference path="../typings/jquery/jquery.d.ts" />
+
+declare var g_App;
+
+export class App {
+    router: Router;
+    debugMode = false;
+    editor: FormEditor = null;
+    @bindable model = null;
+
+    constructor() {
+        g_App = this;
+    }
+
+    configureRouter(config, router: Router) {
+        config.title = 'Aurelia';
+        config.map([
+            { route: ['abc', 'welcome'], moduleId: './welcome', nav: true, title: 'Welcome' },
+            { route: ['', 'Editor'], moduleId: './psp/formEditor', nav: true, title: 'Template Editor' }, // TODO: downloaded data from database
+        ]);
+
+        this.router = router;
+    }
+}
